fd3eab7a6c0270948cfa7644ee99de157c2731a7
unknown <na510@exeter.ac.uk>
2023-11-27 21:44:47 +0000

    added encoding and splitting the data, and then the knn training code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from preprocessing import *
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
 
 if __name__ == '__main__':
     # specify the path for the dataset
@@ -10,4 +12,11 @@ if __name__ == '__main__':
     # at this point, the copied dataset at the specified path is the dataset to work on
 
     # split data into train and test data
-    training_data, testing_data = split_data(dataset_path)
+    x_train, x_test, y_train, y_test = split_data(dataset_path)
+
+    print("Training Model")
+    n = 5
+    knn_model = KNeighborsClassifier(n)
+    knn_model.fit(x_train, y_train)
+    y_pred = knn_model.predict(x_test)
+    print("Accuracy: " + str(metrics.accuracy_score(y_test, y_pred)))
--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from shutil import copy
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
 def create_new_copy(src_path, destination_path):
@@ -24,8 +25,11 @@ def preprocess_csv(filepath):
     # axis specifies to remove columns, not rows
     data.drop(columns=columns_to_remove, inplace=True, axis=1)
 
-    # remove any rows where 'Recall Type' is not vehicle
-    data = data.drop(data[data['Recall Type'] != 'Vehicle'].index)
+    # remove any rows where 'Recall Type' is not vehicle - COMMENTED THIS OUT FOR NOW
+    # data = data.drop(data[data['Recall Type'] != 'Vehicle'].index)
+
+    # remove any rows which contains NaN or no value
+    data.dropna(axis=0, inplace=True)
 
     # save the dataframe as a CSV and overwrite the original file
     data.to_csv(path_or_buf=filepath, index=False)
@@ -33,11 +37,45 @@ def preprocess_csv(filepath):
     print(filepath + " has been preprocessed")
 
 
+def remove_result_column(x_dataset, header):
+    print("Separating the result column - " + str(header))
+    # create new dataframe y_dataset to hold the column specified by the 'header' variable
+    y_dataset = pd.DataFrame(x_dataset[header])
+
+    # remove the column name 'header' from the x_dataset
+    x_dataset.drop(columns=header, inplace=True, axis=1)
+
+    # return the two datasets
+    return x_dataset, y_dataset
+
+
+def encode_data(dataframe):
+    print("Encoding Data...")
+    le = LabelEncoder()
+    dataframe['Manufacturer'] = le.fit_transform(dataframe['Manufacturer'])
+    dataframe['Recall Type'] = le.fit_transform(dataframe['Recall Type'])
+    dataframe['Component'] = le.fit_transform(dataframe['Component'])
+    dataframe['Park Outside Advisory '] = le.fit_transform(dataframe['Park Outside Advisory '])
+    dataframe['Do Not Drive Advisory'] = le.fit_transform(dataframe['Do Not Drive Advisory'])
+
+    dataframe['Report Received Date'] = le.fit_transform(dataframe['Report Received Date'])
+    dataframe['Subject'] = le.fit_transform(dataframe['Subject'])
+
+    return dataframe
+
+
+
 def split_data(filepath):
-    print("Splitting into Training and Testing data")
-    dataset = pd.read_csv(filepath)
-    training_data, testing_data = train_test_split(dataset, test_size=0.2)
-    return training_data, testing_data
+    print("Splitting into Training and Testing data....")
+    x_dataset = pd.read_csv(filepath)
+    x_dataset = encode_data(x_dataset)
+    x_dataset.to_csv(path_or_buf='./data/encoded_data.csv', index=False)
+
+    # remove the 'Do Not Drive Advisory' column from x_dataset and store it in y_dataset
+    x_dataset, y_dataset = remove_result_column(x_dataset, 'Do Not Drive Advisory')
+
+    x_train, x_test, y_train, y_test = train_test_split(x_dataset, y_dataset, test_size=0.2)
+    return x_train, x_test, y_train, y_test
 
 
 def get_unique(header, filepath):
